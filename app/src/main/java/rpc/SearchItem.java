23b8f730dd3b865c2b178d85fbd4a190ed0c3cbe
yuliangwang03 <43104837+yuliangwang03@users.noreply.github.com>
2020-06-18 17:58:19 -0700

    Job Recommendation System

---
+++ app/src/main/java/rpc/SearchItem.java
@@ -0,0 +1,81 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+//import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+import entity.Item;
+import external.GitHubClient;
+
+import org.json.JSONArray;
+
+
+/**
+ * Servlet implementation class SearchItem
+ */
+public class SearchItem extends HttpServlet {
+	//Data used by Tomcat to maintain Servlet internally
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		//
+		String userId = request.getParameter("user_id");
+		double lon = -122.08;
+		double lat = 37.38;
+		//double lat = Double.parseDouble(request.getParameter("lat"));
+		//double lon = Double.parseDouble(request.getParameter("lon"));
+
+		GitHubClient client = new GitHubClient();
+		List<Item> items = client.search(lat, lon, null);
+
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+		//Get the JSONArray through the serach method and write it to the response.
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			JSONObject obj = item.toJSONObject();
+			obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
